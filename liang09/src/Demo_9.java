bc340468f82ec62dd944ed3e52db99d5c06c8320
moogle <vr@riseup.net>
2015-11-12 13:35:43 +0800

    calculation

--- liang09/src/Demo_9.java
+++ liang09/src/Demo_9.java
@@ -2,7 +2,26 @@
 public class Demo_9 {
   public static void main(String[] args) {
     for (int i = 0; i < args.length; i++) {
-      System.out.println(args[i]);
+      System.out.println(i+": ---"+args[i]+"---");
+    }
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[2]);
+    if (args[1].charAt(0) == '+') {
+      System.out.println(args[0]+" + "+args[2]+" = "+(a+b));
+    }
+    else if (args[1].charAt(0) == '-') {
+      System.out.println(args[0]+" - "+args[2]+" = "+(a-b));
+    }
+    else if (args[1].charAt(0) == '*') {
+      System.out.println(args[0]+" * "+args[2]+" = "+(a*b));
+    }
+    else if (args[1].charAt(0) == '/') {
+      System.out.println(args[0]+" / "+args[2]+" = "+(a/b));
+    }
+    
+    else {
+      System.out.println("else block.");
+      System.out.println("nothing");
     }
   }
 }
